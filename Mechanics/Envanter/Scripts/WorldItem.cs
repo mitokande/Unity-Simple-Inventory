20bc19b9fde6799b56d5768bde97b42caf1415a3
Mithat Can Turan <benmithat18@gmail.com>
2021-07-28 23:03:38 +0300

    Changed the way of taking item

--- Mechanics/Envanter/Scripts/WorldItem.cs
+++ Mechanics/Envanter/Scripts/WorldItem.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class WorldItem : MonoBehaviour
 {
     public Item item;
+    public int amount;
     // Start is called before the first frame update
     void Start()
     {
